8c4ffcce37578d433a72019bb11e6809cb3b6b8e
BurakCaglar <burakcaglar@yandex.com>
2021-02-12 22:58:58 +0300

    local storage added

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStroge = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStroge());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -65,6 +74,10 @@ function App() {
     setEditID(id);
     setName(specificItem.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <section className="section-center">
